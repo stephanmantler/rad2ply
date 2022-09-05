96e19e7dd59f64bf2f772d9bc43cad89955cbd1d
Stephan Mantler <step@stephanmantler.com>
2022-09-05 07:26:05 +0000

    added reprojection to local X/Y instead of fudging lat/lon values

--- makeply.py
+++ makeply.py
@@ -6,10 +6,13 @@
 import pickle
 import numpy as np
 
+# for reprojection
+from osgeo import ogr,osr
+
 inputFileName = '20220901_flight2_full_upsample10.pickle'
 outputFileName = 'scratch.ply'
-#inputPicke = '20220901_flight2_full.pickle'
-#inputPicke = '20220901_flight2.pickle'
+#inputFileName = '20220901_flight2_full.pickle'
+inputFileName = '20220901_flight2.pickle'
 
 # -- parameters to tune output --
 
@@ -23,9 +26,56 @@ minReturnPower = -70
 maxReturnPower = -40
 
 # scaling factor for latitude / longitude and vertical
-latitudeScale = 10000
-longitudeScale = 3000
-verticalScale = 0.15
+horizontalScale = 0.01
+verticalScale = 0.015
+
+# local orthographic projection origin
+# useful for aligning multiple data sets. reprojection assumes input data is EPSG:4326 (WGS84 lat/lon)
+# leave as np.nan to use first data point as origin
+localOriginLat = np.nan
+localOriginLon = np.nan
+
+# --- local functions ---
+
+# make local reference system
+def make_local_reference():
+    if np.isnan(localOriginLat) or np.isnan(localOriginLon):
+        return None
+    sr = osr.SpatialReference()
+    projection = '+proj=ortho +lat_0=%0.7f +lon_0=%0.7f +datum=WGS84 +units=m +no_defs ' % (localOriginLat, localOriginLon)
+    sr.ImportFromProj4(projection)
+    return sr
+
+# convert return power to RGBA color
+# we just clamp to desired range and convert to greyscale.
+def rp_to_color(power):
+    # -70 to -40 to start with.
+    scaled = (power - minReturnPower) / (maxReturnPower - minReturnPower)
+    if scaled < 0:
+        scaled = 0
+    if scaled > 1:
+        scaled = 1
+    return (scaled * 255, scaled * 255, scaled * 255, 255)
+
+coordTransform = None
+
+# reproject lat/lon to local meters (ignoring Z values)
+def reproject(lat, lon):
+    # calculate coordinate transform on first run only
+    if coordTransform is None:
+        if np.isnan(lat) or np.isnan(lon):
+            return [None, None]
+        source = osr.SpatialReference()
+        source.ImportFromEPSG(4326)
+
+        coordTransform = osr.CoordinateTransformation(source, localSpatialReference)
+
+    return coordTransform.TransformPoint(lat, lon, 0)[:2]
+
+
+# === main code ===
+
+localSpatialReference = make_local_reference()
 
 print("loading pickle from",inputFileName)
 
@@ -58,17 +108,6 @@ plyFaces = []
 lat0 = np.nan
 lon0 = np.nan
 
-# convert return power to RGBA color
-# we just clamp to desired range and convert to greyscale.
-def rp_to_color(power):
-    # -70 to -40 to start with.
-    scaled = (power - minReturnPower) / (maxReturnPower - minReturnPower)
-    if scaled < 0:
-        scaled = 0
-    if scaled > 1:
-        scaled = 1
-    return (scaled * 255, scaled * 255, scaled * 255, 255)
-
 
 print("building geometry ...")
 
@@ -77,17 +116,18 @@ lastBaseIndex = -1
 
 for i in range(timeStepCount):
     
-    # locally rebase all coordinates to the first position we have
-    if np.isnan(lat0) and not np.isnan(data['lat'][i]):
-        lat0 = data['lat'][i]
-        lon0 = data['lon'][i]
-        print("local origin set to", lat0, lon0)
+    if localSpatialReference is None and not np.isnan(data['lat'][i]):
+        localOriginLat = data['lat'][i]
+        localOriginLon = data['lon'][i]
+        localSpatialReference = make_local_reference()
+
     
-    lat = (data['lat'][i] - lat0) * latitudeScale
-    lon = (data['lon'][i] - lon0) * longitudeScale
+ #   lat = (data['lat'][i] - lat0) * latitudeScale
+ #   lon = (data['lon'][i] - lon0) * longitudeScale
+    (x, y) = reproject(data['lat'][i], data['lon'][i])
     
     # skip samples with invalid positioning
-    if np.isnan(lat):
+    if x is None or y is None:
         continue
         
     baseAltitude = data['alt_rel'][i]
@@ -95,7 +135,7 @@ for i in range(timeStepCount):
 
     for d in range(dtrmin, dtrmax):
         rp = data["return_power"][d, i]
-        plyVertices.append( { "v": ( lon, lat, verticalScale * (baseAltitude - dtr[d])), "color": rp_to_color(rp) } )
+        plyVertices.append( { "v": ( horizontalScale * x, horizontalScale * y, verticalScale * (baseAltitude - dtr[d])), "color": rp_to_color(rp) } )
 
     # only start adding faces if we have at least two columns of vertices
     if lastBaseIndex >= 0:
