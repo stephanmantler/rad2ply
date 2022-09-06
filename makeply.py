2b5ee88cc27d0b13010088ead56e1c6614fddc90
Stephan Mantler <step@stephanmantler.com>
2022-09-06 15:09:41 +0000

    direct Blender integration - now works as a file loader addon, loading data without going through an external PLY file. also creates suitable material nodes

--- makeply.py
+++ makeply.py
@@ -3,10 +3,14 @@
 # written on a stormy day at Grímsfjall (N 64.397 W 17.274)
 # stephan mantler / step@stepman.is
 
+from asyncore import write
 import pickle
 import numpy as np
 
+# for reprojection
 import math
+
+# -- these only make sense if run directly, and are ignored when run from within Blender
 
 inputFileName = '20220901_flight2_full_upsample10.pickle'
 outputFileName = 'scratch.ply'
@@ -48,7 +52,8 @@ def rp_to_color(power):
     return (scaled * 255, scaled * 255, scaled * 255)
 
 
-# directly coded orthographic projection based on EPSG:
+# directly coded orthographic projection.
+# Formula and values from EPSG Geomatics Guidance Note Number 7, part 2
 def reproject(lat, lon):
     # semi-major axis
     a = 6378137
@@ -80,122 +85,300 @@ def reproject(lat, lon):
 
 # === main code ===
 
-print("loading pickle from",inputFileName)
+def readPickle(filename):
+    global localOriginLat
+    global localOriginLon
 
-with open(inputFileName, 'rb') as f:
-    data = pickle.load(f)
+    print("loading pickle from",filename)
 
-# assuming all arrays have the same shape so we pull total sample count from latitude
-timeStepCount = len(data['lat'])
+    with open(filename, 'rb') as f:
+        data = pickle.load(f)
 
-# figure out distance range
-# (we will disregard power values that are too close / too far away)
-dtr = data['distance_to_reflector']
-distanceCount = len(dtr)
+    # assuming all arrays have the same shape so we pull total sample count from latitude
+    timeStepCount = len(data['lat'])
 
-dtrmin = 0
-dtrmax = distanceCount
-for i in range(distanceCount):
-    if dtr[i] < distanceLimitNear:
-        dtrmin = max(i, dtrmin)
-    if dtr[i] > distanceLimitFar:
-        dtrmax = min(i, dtrmax)
+    # figure out distance range
+    # (we will disregard power values that are too close / too far away)
+    dtr = data['distance_to_reflector']
+    distanceCount = len(dtr)
 
-dtrUsedCount = dtrmax - dtrmin
+    dtrmin = 0
+    dtrmax = distanceCount
+    for i in range(distanceCount):
+        if dtr[i] < distanceLimitNear:
+            dtrmin = max(i, dtrmin)
+        if dtr[i] > distanceLimitFar:
+            dtrmax = min(i, dtrmax)
 
-print(timeStepCount, "time steps, using range bins", dtrmin, "-", dtrmax)
+    dtrUsedCount = dtrmax - dtrmin
 
-plyVertices = []
-plyFaces = []
+    print(timeStepCount, "time steps, using range bins", dtrmin, "-", dtrmax)
 
-print("building geometry ...")
+    plyVertices = []
+    plyFaces = []
 
-# easy cheat to make building geometry and indexing into the vertex array easier
-lastBaseIndex = -1
+    print("building geometry ...")
 
-for i in range(timeStepCount):
+    # easy cheat to make building geometry and indexing into the vertex array easier
+    lastBaseIndex = -1
 
-    # early break if we don't have data here
-    if np.isnan(data['lat'][i]):
-        continue
-    
-    if np.isnan(localOriginLat):
-        localOriginLat = data['lat'][i]
-        localOriginLon = data['lon'][i]
+    for i in range(timeStepCount):
 
-    
- #   lat = (data['lat'][i] - lat0) * latitudeScale
- #   lon = (data['lon'][i] - lon0) * longitudeScale
-    (x, y) = reproject(data['lat'][i], data['lon'][i])
-    
-    # skip samples with invalid positioning
-    if np.isnan(x) or np.isnan(y):
-        continue
+        # early break if we don't have data here
+        if np.isnan(data['lat'][i]):
+            continue
         
-    baseAltitude = data['alt_rel'][i]
-    baseIndex = len(plyVertices)
+        if np.isnan(localOriginLat):
+            localOriginLat = data['lat'][i]
+            localOriginLon = data['lon'][i]
 
-    for d in range(dtrmin, dtrmax):
-        rp = data["return_power"][d, i]
-        plyVertices.append( { 
-            "v": ( horizontalScale * x, horizontalScale * y, verticalScale * (baseAltitude - dtr[d])), 
-            "color": rp_to_color(rp),
-            "returnPower": rp } )
+        
+    #   lat = (data['lat'][i] - lat0) * latitudeScale
+    #   lon = (data['lon'][i] - lon0) * longitudeScale
+        (x, y) = reproject(data['lat'][i], data['lon'][i])
+        
+        # skip samples with invalid positioning
+        if np.isnan(x) or np.isnan(y):
+            continue
+            
+        baseAltitude = data['alt_rel'][i]
+        baseIndex = len(plyVertices)
 
-    # only start adding faces if we have at least two columns of vertices
-    if lastBaseIndex >= 0:
-        for d in range(0, dtrUsedCount-1):            
-            plyFaces.append( [ baseIndex + d, baseIndex + d + 1, lastBaseIndex + d + 1, lastBaseIndex + d] )
-    lastBaseIndex = baseIndex  
+        for d in range(dtrmin, dtrmax):
+            rp = data["return_power"][d, i]
+            plyVertices.append( { 
+                "v": ( horizontalScale * x, horizontalScale * y, verticalScale * (baseAltitude - dtr[d])), 
+                "color": rp_to_color(rp),
+                "returnPower": rp } )
 
-# write everything out
-print(" -",len(plyFaces),"faces generated")
-print("writing PLY file ...")
+        # only start adding faces if we have at least two columns of vertices
+        if lastBaseIndex >= 0:
+            for d in range(0, dtrUsedCount-1):            
+                plyFaces.append( [ baseIndex + d, baseIndex + d + 1, lastBaseIndex + d + 1, lastBaseIndex + d] )
+        lastBaseIndex = baseIndex  
 
-with open(outputFileName, "wb") as file:
-    fw = file.write
+    # write everything out
+    print(" -",len(plyFaces),"faces generated")
+    return (plyVertices, plyFaces)
 
-    fw(b"ply\n")
-    fw(b"format ascii 1.0\n")
-    
-    fw(b"element vertex %d\n" % len(plyVertices))
-    fw(
-        b"property float x\n"
-        b"property float y\n"
-        b"property float z\n"
+def writePly(filename, vertices, faces):
+    print("writing PLY file ...")
+
+    with open(filename, "wb") as file:
+        fw = file.write
+
+        fw(b"ply\n")
+        fw(b"format ascii 1.0\n")
+        
+        fw(b"element vertex %d\n" % len(vertices))
+        fw(
+            b"property float x\n"
+            b"property float y\n"
+            b"property float z\n"
+        )
+        fw(
+            b"property uchar red\n"
+            b"property uchar green\n"
+            b"property uchar blue\n"
+            b"property float returnPower\n"
+        )
+        
+        fw(b"element face %d\n" % len(faces))
+        fw(b"property list uchar uint vertex_indices\n")
+        fw(b"end_header\n")
+        
+        # Vertex data
+        # ---------------------------
+        
+        print(" ... vertex data ...")
+        for v in vertices:
+            fw(b"%.6f %.6f %.6f" % v['v'][:])
+            fw(b" %u %u %u" % v['color'][:])
+            fw(b" %.4f" % v['returnPower'])
+            fw(b"\n")
+        
+        # Face data
+        # ---------------------------
+        
+        print(" ... face data ...")
+        for pf in faces:
+            fw(b"%d" % len(pf))
+            for index in pf:
+                fw(b" %d" % index)
+            fw(b"\n")
+            
+        print(" ... flushing output buffers ...")
+        file.flush()
+            
+
+
+if __name__ == "__main__":
+    (vertices, faces) = readPickle(inputFileName)
+    writePly(outputFileName, vertices, faces)
+    print("all done. enjoy!")
+
+# ===== Blender Integration =====
+
+# if this fails we're running outside of Blender
+# and there is nothing else to do here.
+try:
+    import bpy
+    from bpy.props import (
+        CollectionProperty,
+        StringProperty,
+        BoolProperty,
+        FloatProperty,
     )
-    fw(
-        b"property uchar red\n"
-        b"property uchar green\n"
-        b"property uchar blue\n"
-        b"property float returnPower\n"
+    from bpy_extras.io_utils import (
+        ImportHelper,
+        ExportHelper,
+        axis_conversion,
+        orientation_helper,
     )
-    
-    fw(b"element face %d\n" % len(plyFaces))
-    fw(b"property list uchar uint vertex_indices\n")
-    fw(b"end_header\n")
-    
-    # Vertex data
-    # ---------------------------
-    
-    print(" ... vertex data ...")
-    for v in plyVertices:
-        fw(b"%.6f %.6f %.6f" % v['v'][:])
-        fw(b" %u %u %u" % v['color'][:])
-        fw(b" %.4f" % v['returnPower'])
-        fw(b"\n")
-    
-    # Face data
-    # ---------------------------
-    
-    print(" ... face data ...")
-    for pf in plyFaces:
-        fw(b"%d" % len(pf))
-        for index in pf:
-            fw(b" %d" % index)
-        fw(b"\n")
-        
-    print(" ... flushing output buffers ...")
-    file.flush()
-        
-print("all done. enjoy!")
+except:
+    exit()
+
+bl_info = {
+    "name": "Stanford Glacier Radiometry Import",
+    "author": "Stephan Mantler",
+    "location": "File > Import/Export",
+    "version": (2, 2, 0),
+    "blender": (3, 0, 0),
+    "description": "Import Stanford Glacier Radiometry data",
+    "category": "Import-Export",
+}
+
+class ImportStanfordRadiometry(bpy.types.Operator, ImportHelper):
+    """Load a PLY geometry file"""
+    bl_idname = "import_mesh.stanfordradiometry"
+    bl_label = "Import Stanford Glacier Radiometry"
+    bl_options = {'UNDO'}
+
+    files: CollectionProperty(
+        name="File Path",
+        description="File path used for importing the radiometry file",
+        type=bpy.types.OperatorFileListElement,
+    )
+
+    # Hide opertator properties, rest of this is managed in C. See WM_operator_properties_filesel().
+#    hide_props_region: BoolProperty(
+#        name="Hide Operator Properties",
+#        description="Collapse the region displaying the operator settings",
+#        default=True,
+#    )
+
+    directory: StringProperty()
+
+    filename_ext = ".pickle"
+    filter_glob: StringProperty(default="*.pickle", options={'HIDDEN'})
+
+    def execute(self, context):
+        import os
+        import bmesh
+
+        context.window.cursor_set('WAIT')
+
+        paths = [
+            os.path.join(self.directory, name.name)
+            for name in self.files
+        ]
+
+        if not paths:
+            paths.append(self.filepath)
+
+        for ob in bpy.context.selected_objects:
+            ob.select_set(False)
+
+        for path in paths:
+            name = bpy.path.display_name_from_filepath(path)
+
+            print(" -- import:", path)
+            (vertices, faces) = readPickle(path)
+            mesh_data = bpy.data.meshes.new("cube_mesh_data")
+            mesh_data.from_pydata([v['v'] for v in vertices], [], faces)
+            mesh_data.update()
+
+            bm = bmesh.new()
+            bm.from_mesh(mesh_data)
+            layer_kind = bm.verts.layers.float.new("returnPower")
+            bm.verts.ensure_lookup_table()
+            for i, v in enumerate(vertices):
+                bm.verts[i][layer_kind] = v['returnPower']
+
+            bm.to_mesh(mesh_data)
+            bm.free()
+
+            obj = bpy.data.objects.new(name, mesh_data)
+            bpy.context.collection.objects.link(obj)
+            bpy.context.view_layer.objects.active = obj
+            obj.select_set(True)
+
+            # create suitable material
+            material = bpy.data.materials.new(name="mat_"+name)
+            material.use_nodes = True
+
+            # Remove default
+            material.node_tree.nodes.remove(material.node_tree.nodes.get('Principled BSDF')) #title of the existing node when materials.new
+            material_output = material.node_tree.nodes.get('Material Output')
+
+            attribute_node = material.node_tree.nodes.new('ShaderNodeAttribute')
+            attribute_node.location.x = -600
+            attribute_node.attribute_name="returnPower"
+
+            map_range = material.node_tree.nodes.new('ShaderNodeMapRange')
+            map_range.location.x = -400
+            map_range.inputs[1].default_value = -70
+            map_range.inputs[2].default_value = -40
+
+            color_ramp = material.node_tree.nodes.new("ShaderNodeValToRGB")
+            cr = color_ramp.color_ramp
+            color_ramp.location.x = -200
+            cr.elements.new(position = 0.14)
+            cr.elements.new(position = 0.29)
+            cr.elements.new(position = 0.43)
+            cr.elements.new(position = 0.57)
+            cr.elements.new(position = 0.71)
+            cr.elements.new(position = 0.86)
+
+            cr.elements[0].color=(68/255,1/255,84/255,1)
+            cr.elements[1].color=(70/255,50/255,127/255,1)
+            cr.elements[2].color=(54/255,92/255,141/255,1)
+            cr.elements[3].color=(39/255,127/255,142/255,1)
+            cr.elements[4].color=(31/255,161/255,135/255,1)
+            cr.elements[5].color=(74/255,194/255,109/255,1)
+            cr.elements[6].color=(159/255,218/255,58/255,1)
+            cr.elements[7].color=(253/255,231/255,3/255,1)
+
+            diffuse = material.node_tree.nodes.new('ShaderNodeBsdfDiffuse')    #name of diffuse BSDF when added with shift+A
+            diffuse.location.x = 200
+
+            material.node_tree.links.new(map_range.inputs[0], attribute_node.outputs[2])
+            material.node_tree.links.new(color_ramp.inputs[0], map_range.outputs[0])
+            material.node_tree.links.new(diffuse.inputs[0], color_ramp.outputs[0])
+
+            # link diffuse shader to material
+            material.node_tree.links.new(material_output.inputs[0], diffuse.outputs[0])
+
+            # set activer material to your new material
+            bpy.context.object.active_material = material
+
+            context.window.cursor_set('DEFAULT')
+
+            return {'FINISHED'}
+
+
+def menu_func_import(self, context):
+    self.layout.operator(ImportStanfordRadiometry.bl_idname, text="Stanford Glacier Radiometry (.pickle)")
+
+
+def register():
+    bpy.utils.register_class(ImportStanfordRadiometry)
+
+    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+
+
+def unregister():
+    bpy.utils.unregister_class(ImportStanfordRadiometry)
+
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+
